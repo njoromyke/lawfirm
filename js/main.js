671a87d24111f21b62b5b38415e177e445c2cef9
ngahika01 <mykenjoroge01@gmail.com>
2021-01-26 18:50:55 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// import counterUp from 'counterup2'
+
+// const el = document.querySelector( '.counter' )
+
+// // Start counting, do this on DOM ready or with Waypoints.
+// counterUp( el, {
+//     duration: 1000,
+//     delay: 16,
+// } )
+// require( 'waypoints/lib/noframework.waypoints.js' )
+// const el = document.querySelector( '.counter' )
+// new Waypoint( {
+//     element: el,
+//     handler: function() { 
+//         counterUp( el ) 
+//         this.destroy()
+//     },
+//     offset: 'bottom-in-view',
+// } )

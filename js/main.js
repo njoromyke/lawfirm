af5d3fa5f63fe5f6eb35790ae9b00e00eeca6516
njoromyke <mykenjoroge01@gmail.com>
2022-12-22 12:17:28 +0300

    removed unused code and added shadow to navbar on scroll down

--- js/main.js
+++ js/main.js
@@ -1,64 +1,39 @@
-// import counterUp from 'counterup2'
+$(document).ready(function () {
+  "use strict";
+  $(window).on("scroll", function () {
+    var top = $(window).scrollTop();
+    if (top >= 60) {
+      $("nav").addClass("newnav").addClass("shadow-sm").fadeIn(2000);
+    } else if ($("nav").hasClass("newnav") && $("nav").hasClass("shadow-sm")) {
+      $("nav").removeClass("newnav") && $("nav").removeClass("shadow-sm");
+    }
+  });
 
-// const el = document.querySelector( '.counter' )
+  $(".owl-carousel").owlCarousel({
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+        loop: true,
+      },
+      600: {
+        items: 1,
+        nav: true,
+        loop: true,
+      },
+      1000: {
+        items: 1,
+        nav: true,
+        loop: true,
+        autoHeight: true,
+      },
+    },
 
-// // Start counting, do this on DOM ready or with Waypoints.
-// counterUp( el, {
-//     duration: 1000,
-//     delay: 16,
-// } )
-// require( 'waypoints/lib/noframework.waypoints.js' )
-// const el = document.querySelector( '.counter' )
-// new Waypoint( {
-//     element: el,
-//     handler: function() { 
-//         counterUp( el ) 
-//         this.destroy()
-//     },
-//     offset: 'bottom-in-view',
-// } )
-
-$(document).ready(function(){
-    $(window).on('scroll',function(){
-        var top = $(window).scrollTop();
-        if(top>=60){
-            $("nav").addClass("newnav");
-        }
-        else
-        if($('nav').hasClass('newnav')){
-            $('nav').removeClass("newnav");
-        }
-       
-    });
-
-    $(".owl-carousel").owlCarousel({
-        responsive:{
-            0:{
-                items:1,
-                nav:true,
-                loop:true
-            },
-            600:{
-                items:1,
-                nav:true,
-                loop:true
-            },
-            1000:{
-                items:1,
-                nav:true,
-                loop:true,
-                autoHeight:true,
-            }
-        },
-        
-        autoplay:true,
-        autoplayHoverPause:true,
-        
-        
-    });
-    var scroll = new SmoothScroll('a[href*="#"]',{
-        easing: 'easeInOutCubic', // Easing pattern to use
-        speed: 400, // Integer. Amount of time in milliseconds it should take to scroll 1000px
-    
-    });
+    autoplay: true,
+    autoplayHoverPause: true,
+  });
+  var scroll = new SmoothScroll('a[href*="#"]', {
+    easing: "easeInOutCubic", // Easing pattern to use
+    speed: 400, // Integer. Amount of time in milliseconds it should take to scroll 1000px
+  });
 });
